606b7784e57244b2d9adbb77838048caed8f90d8
oisintohak <oisintohak@gmail.com>
2022-04-08 15:59:05 +0000

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from 'react-bootstrap/Button';
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      Hallo Werld?
+      <NavBar />
     </div>
   );
 }
